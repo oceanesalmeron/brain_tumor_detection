dd1ba5bb956578550e8b034dec9e91a4af02a8c9
Océ <oceane.salmeron@edu.ece.fr>
2021-08-16 22:47:41 +0200

    add build_ dataset function and preprocess img data

---
+++ src/process_data.py
@@ -0,0 +1,55 @@
+"""
+Module implemented to build and pre-process a dataset containing images.
+"""
+
+import os
+
+import cv2
+import pandas as pd
+
+ROOT = './data'
+
+def build_dataset() -> pd.DataFrame:
+    """
+    Build a dataset from mri images.
+
+    Returns
+    -------
+    A Pandas dataframe containing the data.
+    """
+    labels = []
+    paths = []
+
+    for path, _, files in os.walk(ROOT):
+        for name in files:
+            labels.append(os.path.basename(path))
+            paths.append(os.path.join(path, name))
+
+    df = pd.DataFrame({'paths': paths, 'labels': labels})
+    return df.iloc[1: , :]
+
+
+def process_images(img_paths: pd.Series) -> pd.Series:
+    """
+    Pre process images.
+
+    Returns
+    -------
+    A Pandas Series of processed images.
+    """
+    images = []
+
+    for path in img_paths:
+        img = cv2.imread(path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (224,224))
+        images.append(img)
+
+    return images
+
+
+if __name__ == '__main__':
+    data = build_dataset()
+    data['data'] = process_images(data['paths'])
+    data['labels'], labels = data['labels'].factorize()
+    print(data.head())
